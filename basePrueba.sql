20a8373de9878f9c2afbb2a44cdaae922e738c43
CarlosAgustinBrrr <agustin.gacar18@zaragoza.salesianos.edu>
2023-03-23 20:33:55 +0100

    Register va

--- basePrueba.sql
+++ basePrueba.sql
@@ -1,26 +1,28 @@
 CREATE DATABASE IF NOT EXISTS basePrueba;
 use basePrueba;
 CREATE TABLE Usuario(
-dni VARCHAR(9) NOT NULL PRIMARY KEY,
+dni VARCHAR(9) PRIMARY KEY,
 nombre VARCHAR(15) NOT NULL,
+username VARCHAR(15) NOT NULL,
 apellidos VARCHAR(15) NOT NULL,
-email VARCHAR(80) NOT NULL UNIQUE KEY,
-id_ciudad INT NOT NULL,
-fecha_nacimiento DATE NOT NULL,
+email VARCHAR(80) UNIQUE KEY,
+id_ciudad INT NULL,
+fecha_nacimiento DATE,
 descuento INT UNSIGNED,
-fecha_alta DATE NOT NULL,
+fecha_alta DATE,
+contraseña varchar(16) NOT NULL,
 CONSTRAINT UsuarioReserva
 FOREIGN KEY(id_ciudad) REFERENCES Ciudad (id_ciudad)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE UsuarioActividad(
 dniUsuario varchar(9) NOT NULL PRIMARY KEY,
-idActividad varchar(9) NOT NULL PRIMARY KEY,
+idActividad varchar(9),
 CONSTRAINT Actividad
 FOREIGN KEY(idActividad) REFERENCES Actividad(id),
 CONSTRAINT Usuario
 FOREIGN KEY(dniUsuario) REFERENCES Usuario(dni)
-)ENGINE=Antonio DEFAULT CHARSET=latin1;
+)ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE Actividad(
 id varchar(9) NOT NULL PRIMARY KEY,
@@ -28,9 +30,10 @@ idCategoria varchar(9) NOT NULL,
 descripcion varchar(2000) NOT NULL,
 CONSTRAINT Categoria
 FOREIGN KEY(idCategoria)REFERENCES Categoria(id)
-)ENGINE=Antonio DEFAULT CHARSET=latin1;
+)ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE Categoria(
 id varchar(9) NOT NULL PRIMARY KEY,
 tipo varchar(20) NOT NULL
-)ENGINE=Antonio DEFAULT CHARSET=latin1;
+
+)ENGINE=MyISAM DEFAULT CHARSET=latin1;
