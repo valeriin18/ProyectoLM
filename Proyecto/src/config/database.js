e915f537842dfa1d90dba2952b37ae1ac28fddc8
CarlosAgustinBrrr <agustin.gacar18@zaragoza.salesianos.edu>
2023-03-23 19:50:37 +0100

    register añadido

--- Proyecto/src/config/database.js
+++ Proyecto/src/config/database.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 // npm install sequelize
 // npm install mysql2
 
-const db = new Sequelize('basePrueba', 'root', '', {
+const db = new Sequelize('baseDeDatosPractica1', 'root', '', {
     host : "localhost",
     dialect : "mysql"
 });
