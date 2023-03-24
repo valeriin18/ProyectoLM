687112060704d5913322f1fbc73258bcb56b8635
valeriin18 <valero.ladan18@zaragoza.salesianos.edu>
2023-03-24 19:34:06 +0100

    dss

--- Proyecto/src/routes/index.js
+++ Proyecto/src/routes/index.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { Register } from "../controllers/users.js";
 const router = express.Router();
 
 router.get('/', (req, res) => {    
     res.render('pages/index');
 });
-
+router.post('/register',Register);
 export default router;
