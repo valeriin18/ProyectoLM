ff8e0003b649ac56b31c9bb5efc4718a25b8e7a1
ernavajadebronce <antonioberche44@gmail.com>
2023-05-02 16:35:18 +0200

    estilo

--- ProyectoPlenaInclusion/client/src/components/Login.js
+++ ProyectoPlenaInclusion/client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import "./style.css";
 const Login = () => {
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
@@ -31,38 +31,67 @@ const Login = () => {
                 }
         }
     }
-    return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column is-4-desktop">
-                            <form onSubmit={Auth} className="box">
-                                <div className="field mt-5 has-text-centered">
-                                    <p className="has-text-centered" style={{ fontSize: 45 }}>PlenaInclusión</p>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Email</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Username" value={mail} onChange={(e) => setMail(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Password</label>
-                                    <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <button className="button is-success is-fullwidth">Login</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+
+    const renderForm = (
+        <div className="form">
+        <form onSubmit={Auth}>
+            <div className="input-container">
+            <label>Username </label>
+            <input type="text" name="uname" required />
+
             </div>
-        </section>
-    )
+            <div className="input-container">
+            <label>Password </label>
+            <input type="password" name="pass" required />
+
+            </div>
+            <div className="button-container">
+                <input type="submit" />
+            </div>
+        </form>
+        </div>
+    );
+    
+    return (
+    <div className="app">
+        <div className="login-form">
+        <div className="title">Sign In</div>
+            {renderForm}
+        </div>
+    </div>
+    );
+    // return (
+    //     <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+    //         <div className="hero-body">
+    //             <div className="container">
+    //                 <div className="columns is-centered">
+    //                     <div className="box is-flex justify-content-center">
+    //                         <form onSubmit={Auth} className="box">
+    //                             <div className="field mt-5 has-text-centered">
+    //                                 <p className="has-text-centered" style={{ fontSize: 45, color: '#201ad8' }}>PlenaInclusión</p>
+    //                             </div>
+    //                             <div className="field mt-5">
+    //                                 <label className="label">Email</label>
+    //                                 <div className="controls">
+    //                                     <input type="text" className="input" placeholder="Username" value={mail} onChange={(e) => setMail(e.target.value)} />
+    //                                 </div>
+    //                             </div>
+    //                             <div className="field mt-5">
+    //                                 <label className="label">Password</label>
+    //                                 <div className="controls">
+    //                                     <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+    //                                 </div>
+    //                             </div>
+    //                             <div className="field mt-5">
+    //                                 <button className="button is-success is-fullwidth">Login</button>
+    //                             </div>
+    //                         </form>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </section>
+    // )
 }
 
 export default Login
