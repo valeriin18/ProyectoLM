4641d8c233cc6de2667f27949b2ceeca643d95eb
valeriin18 <valero.ladan18@zaragoza.salesianos.edu>
2023-05-18 20:30:49 +0200

    sdsd

--- ProyectoPlenaInclusion/client/src/components/Login.js
+++ ProyectoPlenaInclusion/client/src/components/Login.js
@@ -3,66 +3,71 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [mail, setMail] = useState('');
-    const [password, setPassword] = useState('');
-    const [msg, setMsg] = useState('');
-    const history = useNavigate();
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const history = useNavigate();
 
-    const Auth = async (e) => {
-        e.preventDefault();
+  const Auth = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/loginProfessional', {
+        mail: mail,
+        password: password
+      });
+      history("/dashboard");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      } else {
         try {
-            await axios.post('/loginProfessional', {
-                mail: mail,
-                password: password
-            });
-            history("/dashboard");
+          await axios.post('/loginCustomer', {
+            mail: mail,
+            password: password
+          });
+          history("/dashboard");
         } catch (error) {
-            if (error.response) {
-                setMsg(error.response.data.msg);
-            } 
-                try {
-                    await axios.post('/loginCustomer', {
-                        mail: mail,
-                        password: password
-                    });
-                    history("/dashboard");
-                } catch (error) {
-                    window.alert("Error de autenticación. Verifique sus credenciales e inténtelo de nuevo.");
-                }
+          window.alert("Error de autenticación. Verifique sus credenciales e inténtelo de nuevo.");
         }
+      }
     }
-    return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column is-4-desktop">
-                            <form onSubmit={Auth} className="box">
-                                <div className="field mt-5 has-text-centered">
-                                    <p className="has-text-centered" style={{ fontSize: 45 }}>PlenaInclusión</p>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Email</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Username" value={mail} onChange={(e) => setMail(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Password</label>
-                                    <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <button className="button is-success is-fullwidth">Login</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+  }
+
+  return (
+    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-4-desktop">
+              <form onSubmit={Auth} className="box">
+                <div className="field mt-5 has-text-centered">
+                  <p className="has-text-centered" style={{ fontSize: 45 }}>PlenaInclusión</p>
                 </div>
+                <div className="field mt-5">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input type="text" className="input" placeholder="Username" value={mail} onChange={(e) => setMail(e.target.value)} />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Password</label>
+                  <div className="control">
+                    <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <button className="button is-success is-fullwidth">Login</button>
+                </div>
+                <div className="field mt-3">
+                  {msg && <p className="help is-danger">{msg}</p>}
+                </div>
+              </form>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default Login
+export default Login;
--- ProyectoPlenaInclusion/client/src/components/Profile.js
+++ ProyectoPlenaInclusion/client/src/components/Profile.js
@@ -2,118 +2,119 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function UserInformation() {
-    const [mail, setMail] = useState("");
-    const [customers, setCustomers] = useState([]);
-    const [professionals, setProfessionals] = useState([]);
-    const [newMail, setNewMail] = useState("");
-    const [newPassword, setNewPassword] = useState("");
+  const [mail, setMail] = useState("");
+  const [customers, setCustomers] = useState([]);
+  const [professionals, setProfessionals] = useState([]);
+  const [newMail, setNewMail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const [customersRes, professionalsRes] = await Promise.all([
-                axios.post("/GetUsers", { mail }),
-                axios.post("/GetProfessionals", { mail })
-            ]);
-            setCustomers(customersRes.data);
-            setProfessionals(professionalsRes.data);
-            console.log(customersRes.data)
-            console.log(professionalsRes.data)
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const [customersRes, professionalsRes] = await Promise.all([
+        axios.post("/GetUsers", { mail }),
+        axios.post("/GetProfessionals", { mail })
+      ]);
+      setCustomers(customersRes.data);
+      setProfessionals(professionalsRes.data);
+      console.log(customersRes.data)
+      console.log(professionalsRes.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const handleUpdate = async (e, user) => {
-        e.preventDefault();
-        try {
-            const { idCustomer, mail, password } = user;
-            let res;
-            if (idCustomer) {
-                res = await axios.post("/updateCustomer", {
-                    idCustomer,
-                    newMail: newMail || mail,
-                    newPassword: newPassword || password,
-                });
-                setCustomers(customers.map((u) => {
-                    if (u.idCustomer === user.idCustomer) {
-                        return { ...u, mail: newMail || u.mail };
-                    } else {
-                        return u;
-                    }
-                }));
-            } else {
-                const { idProfessional, mail, password } = user;
-                let res1;
-                res1 = await axios.post("/updateProfessional", {
-                    idProfessional,
-                    newMail: newMail || mail,
-                    newPassword: newPassword || password,
-                });
-                setProfessionals(professionals.map((u) => {
-                    if (u.idProfessional === user.idProfessional) {
-                        return { ...u, mail: newMail || u.mail };
-                    } else {
-                        return u;
-                    }
-                }));
-            }
-            console.log(res.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Mail:
-                    <input type="text" value={mail} onChange={(e) => setMail(e.target.value)} />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
-            {customers.map((user) => (
-                <div key={user.idCustomer}>
-                    <h2>{user.name} {user.surname1} {user.surname2}</h2>
-                    <p>ID: {user.idCustomer}</p>
-                    <p>DNI: {user.DNI}</p>
-                    <p>Birth Year: {user.birthyear}</p>
-                    <p>Mail: {user.mail}</p>
-                    <form onSubmit={(e) => handleUpdate(e, user)}>
-                        <label>
-                            Nuevo Mail:
-                            <input type="text" value={newMail} onChange={(e) => setNewMail(e.target.value)} />
-                        </label>
-                        <label>
-                            Nueva Contraseña:
-                            <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-                        </label>
-                        <button type="submit">Actualizar</button>
-                    </form>
-                </div>
-            ))}
-            {professionals.map((user) => (
-                <div key={user.idProfessional}>
-                    <h2>{user.name} {user.surname1} {user.surname2}</h2>
-                    <p>DNI: {user.DNI}</p>
-                    <p>Birth Year: {user.birthyear}</p>
-                    <p>Mail: {user.mail}</p>
-                    <p>Availability: {user.availability}</p>
-                    <form onSubmit={(e) => handleUpdate(e, user)}>
-                        <label>
-                            Nuevo Mail:
-                            <input type="text" value={newMail} onChange={(e) => setNewMail(e.target.value)} />
-                        </label>
-                        <label>
-                            Nueva Contraseña:
-                            <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-                        </label>
-                        <button type="submit">Actualizar</button>
-                    </form>
-                </div>
-            ))}
+  const handleUpdate = async (e, user) => {
+    e.preventDefault();
+    try {
+      const { idCustomer, mail, password } = user;
+      let res;
+      if (idCustomer) {
+        res = await axios.post("/updateCustomer", {
+          idCustomer,
+          newMail: newMail || mail,
+          newPassword: newPassword || password,
+        });
+        setCustomers(customers.map((u) => {
+          if (u.idCustomer === user.idCustomer) {
+            return { ...u, mail: newMail || u.mail };
+          } else {
+            return u;
+          }
+        }));
+      } else {
+        const { idProfessional, mail, password } = user;
+        let res1;
+        res1 = await axios.post("/updateProfessional", {
+          idProfessional,
+          newMail: newMail || mail,
+          newPassword: newPassword || password,
+        });
+        setProfessionals(professionals.map((u) => {
+          if (u.idProfessional === user.idProfessional) {
+            return { ...u, mail: newMail || u.mail };
+          } else {
+            return u;
+          }
+        }));
+      }
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="mail" className="form-label">Mail:</label>
+          <input type="text" className="form-control" id="mail" value={mail} onChange={(e) => setMail(e.target.value)} />
         </div>
-    );
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      {customers.map((user) => (
+        <div key={user.idCustomer} className="my-4">
+          <h2>{user.name} {user.surname1} {user.surname2}</h2>
+          <p>ID: {user.idCustomer}</p>
+          <p>DNI: {user.DNI}</p>
+          <p>Birth Year: {user.birthyear}</p>
+          <p>Mail: {user.mail}</p>
+          <form onSubmit={(e) => handleUpdate(e, user)}>
+            <div className="mb-3">
+              <label htmlFor="newMail" className="form-label">Nuevo Mail:</label>
+              <input type="text" className="form-control" id="newMail" value={newMail} onChange={(e) => setNewMail(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="newPassword" className="form-label">Nueva Contraseña:</label>
+              <input type="password" className="form-control" id="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+            </div>
+            <button type="submit" className="btn btn-primary">Actualizar</button>
+          </form>
+        </div>
+      ))}
+      {professionals.map((user) => (
+        <div key={user.idProfessional} className="my-4">
+          <h2>{user.name} {user.surname1} {user.surname2}</h2>
+          <p>DNI: {user.DNI}</p>
+          <p>Birth Year: {user.birthyear}</p>
+          <p>Mail: {user.mail}</p>
+          <p>Availability: {user.availability}</p>
+          <form onSubmit={(e) => handleUpdate(e, user)}>
+            <div className="mb-3">
+              <label htmlFor="newMail" className="form-label">Nuevo Mail:</label>
+              <input type="text" className="form-control" id="newMail" value={newMail} onChange={(e) => setNewMail(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="newPassword" className="form-label">Nueva Contraseña:</label>
+              <input type="password" className="form-control" id="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+            </div>
+            <button type="submit" className="btn btn-primary">Actualizar</button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default UserInformation;
